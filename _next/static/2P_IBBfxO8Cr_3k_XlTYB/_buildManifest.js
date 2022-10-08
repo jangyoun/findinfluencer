6c3f3e2d88242f398d620e6df0e30b0b10b305bb
jangyoun <jangyounc@gmail.com>
2022-10-09 01:56:02 +0900

    000

---
+++ _next/static/2P_IBBfxO8Cr_3k_XlTYB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1d13f2632292c3b4.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/user":["static/chunks/pages/user-a3346d4b0a7fd837.js"],sortedPages:["/","/_app","/_error","/user"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
