c51571c19d65b3f8317088e85d8ccb23994e9f98
jangyoun <jangyounc@gmail.com>
2022-10-09 23:05:38 +0900

    dynamic header

--- _next/static/W0pXRTK1iRekYt2nvM3TM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-51f70bc2db1ff773.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],"/user":["static/chunks/pages/user-a3346d4b0a7fd837.js"],sortedPages:["/","/_app","/_error","/user"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
